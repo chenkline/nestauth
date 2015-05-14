2881746ecaedf34451d44b91d38d5a1cf80da073
chen.kline <chen.kline@gmail.com>
2015-05-14 20:54:24 +0800

    modify enviroment variable batch script.

--- env.bat
+++ env.bat
@@ -1,9 +1,5 @@
 @echo off
-::echo set nest account chen.kline@gmail.com 
-::SET NEST_ID=c2b1c892-01f6-48da-b53d-f5cc499cff48
-::SET NEST_SECRET=sSaVJHjryjUcI1im95hQAc2og
-
-echo set nest account chen.kline@sohu.com 
+echo set Nest Client ID and Nest Client Secret
 SET NEST_ID=7b5c4cca-b7c3-4787-a3b5-7bf2c4362860
 SET NEST_SECRET=Rt4ui0mTrmyO9kPpecaWR6sNh
 
